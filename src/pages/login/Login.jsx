160d0221542430dec6c5eaa72d02d8d44fc5218c
MALWICK <batahumphrey66@gmail.com>
2023-04-16 02:23:33 +0100

    creating my login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../login/login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            repudiandae sunt assumenda nulla quam temporibus harum ipsum
+            similique accusantium ea?
+          </p>
+          <span>Don't you have an account?</span>
+          <button type="submit">Register</button>
+        </div>
+        <div className="rigth">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="Password" placeholder="Password" />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
