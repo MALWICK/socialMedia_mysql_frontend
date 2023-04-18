9119577c17067cb3764238b347178d5330b8af40
MALWICK <batahumphrey66@gmail.com>
2023-04-18 15:31:59 +0100

    creating my leftbar

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -3,7 +3,20 @@ import "./leftbar.scss";
 
 function Leftbar() {
   return (
-    <div className='leftbar'>Leftbar</div>
+    <div className='leftbar'>
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/10505758/pexels-photo-10505758.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <span>John Doe</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
